a6dc698b1ddbcae641e561d0dc01dddd4bbfbd8f
Tushar <tendrako@uncc.edu>
2023-09-24 11:19:41 -0400

    components added

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent {
+
+}
